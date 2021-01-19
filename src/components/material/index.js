186e504c538b09d724e6ed440fa3ec52ce00736c
Lucas Romero <lucas.romerovivas@gmail.com>
2021-01-19 18:28:06 -0300

    ajustes no front

---
+++ src/components/material/index.js
@@ -0,0 +1,14 @@
+import './material.css'
+
+export default function Material() {
+    return (
+        <div className="material">
+            <div> <img className="material-icon" src={require('../../img/icons/papel.png').default} alt="material Icone" /></div>
+            <div>
+                <span className="material-rotulo">Papel</span><br/>
+                <span className="material-valor">valor</span><span className="material-unidade">unidade</span>
+            </div>
+        </div>
+    )
+}
+
--- src/views/userHome/index.js
+++ src/views/userHome/index.js
@@ -1,9 +1,30 @@
+import MaterialCard from '../../components/materialCard'
+import {PerfilBox} from '../../components/perfil-box'
 
+import './userHome.css'
+
+const itemLista = {
+    perfilType : "coletor",
+    name: "José",
+    location : "Rua xyz, 999",
+    resources : ["plastico", "vidro", "papel", "aluminio"]
+}
 
 export default function UserHome(props) {
     return (
-        <div className="main-logged">
+        <div className="user-home">
+            <MaterialCard rotulo="Recursos Disponíveis"/>
+            <MaterialCard rotulo="Valores estimados para hoje"/>
+            <div id="container-favoritos">
+                <div id="container-filtro">
 
+                </div>
+                <div id="container-lista">
+                    <PerfilBox dados={itemLista} />
+                    <PerfilBox dados={itemLista} />
+                    <PerfilBox dados={itemLista} />
+                </div>
+            </div>
         </div>
     )
 }
