23a575e1ed12b63322c0ce6408901137fd940156
Patrick Araujo <rayssacoosta@gmail.com>
2021-02-17 11:08:07 -0300

    mapbox e tela de agendamentos v1

--- src/components/maps/index.js
+++ src/components/maps/index.js
@@ -1,25 +1,35 @@
 import React, { Component } from "react";
 import Dimensions from "react-dimensions";
-import MapGL from "react-map-gl";
+import MapGL, { NavigationControl } from "react-map-gl";
 import MarkerMap from "./markerMap";
+import Geocoder from "react-map-gl-geocoder";
+
+
+import "mapbox-gl/dist/mapbox-gl.css";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+
 
 const TOKEN =
   "pk.eyJ1IjoiZWNvcmVuZGEiLCJhIjoiY2tsN3kzeTI1MGE2azJxcnVzMnVtdDAzeSJ9.AlzajMLSSV8EUOMQzeQGaw";
   
 class Map extends Component {
-
-  state = {
-    viewport: {
-        latitude: -22.756194,
-        longitude: -43.489972,
-        zoom: 12.8,
-        bearing: 0,
-        pitch: 0,
-        
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewport: {
+                latitude: -22.756194,
+                longitude: -43.489972,
+                zoom: 12.8,
+                bearing: 0,
+                pitch: 0,
+                }
+         };
+    this.mapRef = React.createRef();
     }
+    
+    _updateViewport = viewport => {
+    this.setState({ viewport });
   };
-
-  mapRef = React.createRef();
   
   render() {
     return (
@@ -37,14 +47,21 @@ class Map extends Component {
                     longitude={-43.489972}
                     imgSrc="https://img.icons8.com/color/48/000000/marker.png" imgAlt="Marker"
                     />
+                 
+                <NavigationControl
+                    showCompass={false}
+                    onViewportChange={this._updateViewport}
+                    style={{right:0}}
+                />
                 
-                {/* <Geocoder 
+                <Geocoder 
                     mapRef={this.mapRef}
                     onResult={this.handleOnResult}
                     onViewportChange={this.handleGeocoderViewportChange}
-                    mapboxApiAccessToken={token}
+                    mapboxApiAccessToken={TOKEN}
                     position='top-left'
-                /> */}
+                    placeholder='Busca'
+                />
         </MapGL>
         {/* <DeckGL {...viewport} layers={[searchResultLayer]} /> */}
     </>
