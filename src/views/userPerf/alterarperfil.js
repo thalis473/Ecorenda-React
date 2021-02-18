e8e946ea8128ce753a4d21a02c7edfb8c0f44ce5
thalis473 <tp--thalis@hotmail.com>
2021-02-18 02:12:42 -0300

    rota atualizar cadastro

--- src/views/userPerf/alterarperfil.js
+++ src/views/userPerf/alterarperfil.js
@@ -2,32 +2,54 @@ import React, { useState } from 'react'
 import {useSelector} from 'react-redux'
 import './userPerf.css'
 import { Link } from 'react-router-dom'
+import axios from "axios"
 
 const AtualizarPerfil = () =>{
+    const [usuario, setUsuario] = useState({})
+    const [endereco, setEndereco] = useState({})
     const [material, setMaterial] = useState({})
     const [lista, setLista] = useState({})
-    const handleClick = ({target}) => {
+
+    const user = useSelector(state => state.user.dados)
+    const heardes={
+        'Content-Type': 'application/json',
+        'Authorization': 'JWT fefege...'
+    }
+   
+    /*envio Usuario*/
+    const handleSubmitUsuario = (event) => {
+        axios.post(`http://localhost:4000/alterar/${user.id}`,usuario,{headers:heardes})
+    }
+        /*envio endereços*/
+    const handleSubmitEndereco = (event) => {
+        axios.post('http://localhost:4000/users/cad', endereco,{headers:heardes})
+    }
+        /*envio material*/
+    const handleSubmitMaterial = (event) => {
+        axios.post('http://localhost:4000/users/cad', material,{headers:heardes})
+    }
+
+   
+    const handleChangeEndereco = ({target}) => {
+        const {id, value} = target
+        setEndereco({...endereco, [id]: value})
+        console.log(endereco)
+    }
+
+    
+    const handleChangeUserUsuario = ({target}) => {
+        const {id, value} = target
+        setUsuario({...usuario, [id]: value})
+        console.log(usuario)
+    }
+    const handleChangeMaterial = ({target}) => {
         const {id, textContent} = target
         setMaterial({...material, [id]: textContent})
         setLista({...lista, [id]: textContent })
         console.log(lista)
     }
 
-    const [endereco, setEndereco] = useState({})
-    const handleChange = ({target}) => {
-        const {id, value} = target
-        setEndereco({...endereco, [id]: value})
-        console.log(endereco)
-    }
-
-    const [usuario, setUsuario] = useState({})
-    const handleChangeUser = ({target}) => {
-        const {id, value} = target
-        setUsuario({...usuario, [id]: value})
-        console.log(usuario)
-    }
-
-    const user = useSelector(state => state.user.dados)
+    
 
     return(
     <div className="container">
@@ -38,15 +60,15 @@ const AtualizarPerfil = () =>{
             <form>
                 <div className="form-group">
                     <label htmlFor="nome">Seu Nome</label>
-                    <input onChange={handleChangeUser} type="text" value={user.nome} className="form-control col-sm-9 " id="nome" placeholder="Nome"/>
+                    <input onChange={handleChangeUserUsuario} type="text" className="form-control col-sm-9 " id="nome" placeholder={user.nome}/>
 
                     <label htmlFor="email">E-mail</label>
-                    <input onChange={handleChangeUser} type="text" value={user.email}  id="email" className="form-control col-sm-9"/>
+                    <input onChange={handleChangeUserUsuario} type="text"   id="email" className="form-control col-sm-9"/>
 
                     <label htmlFor="atribuicao">Atribuição</label>
-                    <input onChange={handleChangeUser} type="text" value={user.atribuicao}  id="atribuicao" className="form-control col-sm-9" disabled/>
+                    <input onChange={handleChangeUserUsuario} type="text" value={user.atribuicao}  id="atribuicao" className="form-control col-sm-9" disabled/>
                 </div>
-                <button className="btn btn-success">Salvar dados pessoais</button>
+                <button onClick={handleSubmitUsuario} className="btn btn-success">Salvar dados pessoais</button>
             </form>
         </div>
 
@@ -54,24 +76,24 @@ const AtualizarPerfil = () =>{
         <form>
             <div className="form-group">
                 <label htmlFor="estado">Estado</label>
-                <select onChange={handleChange} id="estado" className="form-control col-sm-3">
+                <select onChange={handleChangeEndereco} id="estado" className="form-control col-sm-3">
                     <option>SELECIONE</option>
                     <option value="rj" >Rio de janeiro</option>
                 </select>
 
                 <label htmlFor="bairro">Bairro</label>
-                <input onChange={handleChange} type="text"  id="bairro" className="form-control col-sm-3"/>
+                <input onChange={handleChangeEndereco} type="text"  id="bairro" className="form-control col-sm-3"/>
 
                 <label htmlFor="rua">Rua</label>
-                <input onChange={handleChange} type="text"  id="rua" className="form-control col-sm-3"/>
+                <input onChange={handleChangeEndereco} type="text"  id="rua" className="form-control col-sm-3"/>
 
                 <label htmlFor="num">Número</label>
-                <input onChange={handleChange} type="text"  id="num" className="form-control col-sm-3"/>
+                <input onChange={handleChangeEndereco} type="text"  id="num" className="form-control col-sm-3"/>
 
                 <label htmlFor="complemento">Complemento</label>
-                <input onChange={handleChange} type="text"  id="complemento" className="form-control col-sm-3"/>
+                <input onChange={handleChangeEndereco} type="text"  id="complemento" className="form-control col-sm-3"/>
             </div>
-            <button className="btn btn-success">Salvar Endereço</button>
+            <button onClick={handleSubmitEndereco} className="btn btn-success">Salvar Endereço</button>
         </form>
 
         <h2>Atualizar Endereço</h2>
@@ -79,15 +101,15 @@ const AtualizarPerfil = () =>{
         <form>
             <ul className="list-group">
 
-                <li onClick={handleClick} className="list-group-item list-group"  >
+                <li onClick={handleChangeMaterial} className="list-group-item list-group"  >
                  <div id='tipoAluminio' className="btn btn-success">aluminio</div>
                 </li>
 
-                <li onClick={handleClick} className="list-group-item list-group" >
+                <li onClick={handleChangeMaterial} className="list-group-item list-group" >
                 <div id='tipoCobre' className="btn btn-success">cobre</div>
                 </li>
 
-                <li onClick={handleClick} className="list-group-item list-group" > 
+                <li onClick={handleChangeMaterial} className="list-group-item list-group" > 
                 <div id='tipoPlastico' className="btn btn-success">plastico</div>
                 </li>
             </ul>
@@ -99,8 +121,8 @@ const AtualizarPerfil = () =>{
                 <li style={{textTransform: "uppercase"}} className="list-group-item list-group-item-success">{lista.tipoPlastico}</li>
             </ul>
         </form>
-        <button className="btn btn-danger"><Link to='/perfil'>Cancelar</Link></button>
+        <button onClick={ handleChangeMaterial} className="btn btn-danger"><Link to='/perfil'>Cancelar</Link></button>
     </div>
     );
 }
-export default AtualizarPerfil;
+export default AtualizarPerfil
