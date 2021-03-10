e15abc8b1a1ce612f53534c4f3b0d07e6c30b672
Lucas Romero <lucas.romerovivas@gmail.com>
2021-03-10 12:59:53 -0300

    refat ajustes no resultado da rota materiais

--- routes/materiais/index.js
+++ routes/materiais/index.js
@@ -46,9 +46,9 @@ materiais.get('/materiais/busca/id=:usuarioId', (req, res, next)=> {
     conn.query(sql, (error, result)=> {
         res.json({
             materiais: [
-                {nome: result[0].fk_aluminioId ? "aluminio" : null, valor: result[0].valorA, peso: result[0].pesoA, descricao: result[0].descA},
-                {nome: result[0].fk_cobreId ? "cobre" : null, valor: result[0].valorC, peso: result[0].pesoC, descricao: result[0].descC},
-                {nome: result[0].fk_plasticoId ? "plastico" : null, valor: result[0].valorP, peso: result[0].pesoP, descricao: result[0].descP}
+                {nome: "aluminio" , valor: result[0].valorA, peso: result[0].pesoA, descricao: result[0].descA},
+                {nome: "cobre", valor: result[0].valorC, peso: result[0].pesoC, descricao: result[0].descC},
+                {nome: "plastico", valor: result[0].valorP, peso: result[0].pesoP, descricao: result[0].descP}
             ]
         })
     })
