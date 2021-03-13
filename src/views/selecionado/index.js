b4cc8241932774f2c30fc36636d2987a4bb411de
Lucas Romero <lucas.romerovivas@gmail.com>
2021-03-13 12:07:36 -0300

    implementação da página de usuario selecionado pela busca

--- src/views/selecionado/index.js
+++ src/views/selecionado/index.js
@@ -3,22 +3,31 @@ import {useSelector, useDispatch} from 'react-redux'
 import {useState} from 'react'
 import axios from 'axios'
 import {rotaPadrao} from '../../dados/fetch'
+import {useHistory} from 'react-router-dom'
+
+import CloseIcon from '@material-ui/icons/Close'
+import MaterialCard from '../../components/materialCard'
 
 import "./selecionado.css"
 
 export default function ViewSelecionado() {
     const selecionado = useSelector(state => state.selecionado)
-    const dispatch = useDispatch
+    const dispatch = useDispatch()
+    const history = useHistory()
     const [user, setUser] = useState(selecionado.dados)
 
     useEffect(() => {
         setUser(selecionado.dados)
+        axios.get(`${rotaPadrao}/materiais/busca/id=${selecionado.dados.id}`)
+        .then(response => dispatch({type: "CARREGAR_MATERIAL", payload: response.data}))
     })
+
 
     return (
     <>
         {user != false ? 
-            (<div className="user-perf">
+            (<div className="user-selecionado">
+            <section><button onClick={()=> history.push("/buscar")} className="btn-fechar"><CloseIcon /></button></section>
             <section>
                 <div className="info-g">
                     <div><img className="profile-icon" src={require(`../../img/icons/person.png`).default} alt="Foto de perfil" /></div>
@@ -50,6 +59,7 @@ export default function ViewSelecionado() {
                     <span>{user.descricao}</span>
                 </div>
             </section>
+            <section><MaterialCard /></section>
         </div>)
         :
         <p>Erro 404</p>
